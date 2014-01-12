89c31474c982a7896f8a5c216a294c6f02621fdb
KRU3L420 <kru3l420@gmail.com>
2014-01-11 19:37:25 -0800

    Update Soraka.lua

--- Soraka.lua
+++ Soraka.lua
@@ -1,3 +1,62 @@
+--[[
+ 
+			Auto Carry Plugin - Soraka: The Starchild
+				Author: KRU3L420
+				Version: See version variables below.
+				Copyright 2014
+
+			Dependency: Sida's Auto Carry
+ 
+			How to install:
+				Make sure you already have AutoCarry installed.
+				Name the script EXACTLY "SidasAutoCarryPlugin - Soraka.lua" without the quotes.
+				Place the Plugin in BoL/Scripts/Common folder.
+
+			Features:
+				Harass with Q
+					- Option to toggle Harass with Q
+				Auto-Carry / Combo
+					- Will Auto-Exhaust Enemy within range
+				Auto-Ignite
+					- Will Ignite on Killable Enemy
+				Auto-E Team
+					- Option to Auto-E Allies
+				Priority / Smart Priority
+					- Option to change Attack Priority
+					- Built-In Priority Sequencer
+					- Will cast W+E on Allies with Lowest Health or Mana
+				Auto-Silence with E
+					- Option to Auto-E enemies to Silence / Interrupt
+				CD Handler
+					- Cooldown Handler for better skill casting
+				Toggleable Options
+					- Many options are able to be turned on or off
+				Plus+
+					- More Features not mentioned above
+					- More to come as well!
+				    
+                
+			Download: 
+				Version History:
+					Version: 1.04
+						Completely rewritten for major release!
+					Version: 1.03
+						Added: Auto-Ignite and Auto-Exhaust
+						Added: CD Handler
+						Added: Priority Sequencer
+						Added: Auto-E Team Option
+						Added: Auto Silence / Interrupt 
+						+Plus More!
+					Version: 1.02
+						Added: Auto-Q Enemy in Auto Carry, and Mixed modes.
+						Added: HotKey to Harass with Q, also moves to cursor.
+						Added: Kill Steal with Q+E
+					Version: 1.01
+						Removed: Damage Check for Astral Blessing(W)
+					Version: 1.00
+                        Release         
+--]]
+
 if myHero.charName ~= "Soraka" then return end
 
 local QReady, WReady, EReady, RReady, IGNITEReady, EXHAUSTReady = nil, nil, nil, nil, nil, nil
@@ -49,26 +108,28 @@ local priorityTable = {
 	},
 }
 
-function OnLoad()
-	Config = scriptConfig("Soraka", "Soraka")
-	Config:addParam("combo", "Silence/Exhaust/Attack Target", SCRIPT_PARAM_ONKEYDOWN, false, 32)
-	Config:addParam("interrupt", "Interrupt with E", SCRIPT_PARAM_ONOFF, true)
-	Config:addParam("printInterrupt", "Print Interrupts", SCRIPT_PARAM_ONOFF, true)
-	Config:addParam("autoIGN", "Auto Ignite", SCRIPT_PARAM_ONOFF, true)
-	Config:addParam("useQ", "Use Q to harass", SCRIPT_PARAM_ONOFF, true)
-	Config:addParam("useW", "Use W", SCRIPT_PARAM_ONOFF, true)
-	Config:addParam("useE", "Use E on ally", SCRIPT_PARAM_ONOFF, true)
-	Config:addParam("minEmana", "Min. E ally mana", SCRIPT_PARAM_SLICE, 50, 0, 100, 0)
-	Config:addParam("autoR", "Use R", SCRIPT_PARAM_ONOFF, true)
-	Config:addParam("minRhealth", "Min. R health limit", SCRIPT_PARAM_SLICE, 15, 0, 100, 0)
-	Config:permaShow("combo")
+function PluginOnLoad()
+	AutoCarry.PluginMenu:addParam("combo", "Silence/Exhaust/Attack", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+	AutoCarry.PluginMenu:addParam("interrupt", "Interrupt with E", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("printInterrupt", "Print Interrupts", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("autoIGN", "Auto Ignite", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("useQ", "Use Q to harass", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("useW", "Use W", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("useE", "Use E on ally", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("minEmana", "Min. E ally mana", SCRIPT_PARAM_SLICE, 50, 0, 100, 0)
+	AutoCarry.PluginMenu:addParam("autoR", "Use R", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("minRhealth", "Min. R health limit", SCRIPT_PARAM_SLICE, 15, 0, 100, 0)
+	AutoCarry.PluginMenu:addParam("autoLvl", "Auto Level Skills (Requires Reload)", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:permaShow("combo")
 
 	ts = TargetSelector(TARGET_PRIORITY, RangeE, DAMAGE_MAGIC)
 	ts.name = "Soraka"
-	Config:addTS(ts)
+	AutoCarry.PluginMenu:addTS(ts)
 
 	IGNITESlot = ((myHero:GetSpellData(SUMMONER_1).name:find("SummonerDot") and SUMMONER_1) or (myHero:GetSpellData(SUMMONER_2).name:find("SummonerDot") and SUMMONER_2) or nil)
 	EXHAUSTSlot = ((myHero:GetSpellData(SUMMONER_1).name:find("SummonerExhaust") and SUMMONER_1) or (myHero:GetSpellData(SUMMONER_2).name:find("SummonerExhaust") and SUMMONER_2) or nil)
+	
+	levelSequence = { nil, 3, 1, 2, 1, 3, 4, 2, 1, 1, 3, 4, 1, 3, 3, 2, 4, 2, 2, }
 	
 	allyTable = GetAllyHeroes()
 	table.insert(allyTable, myHero)
@@ -90,14 +151,16 @@ function OnLoad()
 	end
 end
 
-function OnTick()
+function PluginOnTick()
 	CDHandler()
-	if Config.autoIGN then AutoIgnite() end
-	if Config.autoR then CastR() end
-	if Config.useW then CastW() end
-	if Config.useQ then CastQ() end
-	if Config.useE and not Config.combo then CastE() end
-	if Config.combo then Combo() end
+	if AutoCarry.PluginMenu.autoIGN then AutoIgnite() end
+	if AutoCarry.PluginMenu.autoR then CastR() end
+	if AutoCarry.PluginMenu.useW then CastW() end
+	if AutoCarry.PluginMenu.useQ then CastQ() end
+	if AutoCarry.PluginMenu.useE and not AutoCarry.PluginMenu.combo then CastE() end
+	if AutoCarry.PluginMenu.combo then Combo() end
+	
+	if AutoCarry.PluginMenu.autoLvl then autoLevelSetSequence(levelSequence) end
 end
 
 function CastR()
@@ -106,7 +169,7 @@ function CastR()
 	for i=1, #allyTable do
 		local Ally = allyTable[i]
 
-		if Ally.health/Ally.maxHealth <= Config.minRhealth/100 then
+		if Ally.health/Ally.maxHealth <= AutoCarry.PluginMenu.minRhealth/100 then
 			CastSpell(_R)
 		end
 	end
@@ -164,7 +227,7 @@ function CastE()
 		end
 	end
 
-	if LowestMana and LowestMana.valid and RangeE >= myHero:GetDistance(LowestMana) and LowestMana.mana/LowestMana.maxMana <= Config.minEmana/100 then CastSpell(_E, LowestMana) end
+	if LowestMana and LowestMana.valid and RangeE >= myHero:GetDistance(LowestMana) and LowestMana.mana/LowestMana.maxMana <= AutoCarry.PluginMenu.minEmana/100 then CastSpell(_E, LowestMana) end
 end
 
 function Combo()
@@ -180,12 +243,12 @@ function Combo()
 end
 
 function OnProcessSpell(unit, spell)
-	if #ToInterrupt > 0 and Config.interrupt and EReady then
+	if #ToInterrupt > 0 and AutoCarry.PluginMenu.interrupt and EReady then
 		for _, ability in pairs(ToInterrupt) do
 			if spell.name == ability and unit.team ~= myHero.team then
 				if RangeE >= myHero:GetDistance(unit) then
 					CastSpell(_E, unit)
-					if Config.printInterrupt then print("Tried to interrupt " .. spell.name) end
+					if AutoCarry.PluginMenu.printInterrupt then print("Tried to interrupt " .. spell.name) end
 				end
 			end
 		end
